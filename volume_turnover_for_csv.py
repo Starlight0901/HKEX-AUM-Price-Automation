067ea3f62f6ac74ccb5ba79ac3f780d6a3745d52
Shamini Fernando <126609804+Starlight0901@users.noreply.github.com>
2025-01-16 12:58:14 +0530

    Update volume_turnover_for_csv.py

--- volume_turnover_for_csv.py
+++ volume_turnover_for_csv.py
@@ -126,8 +126,8 @@ def save_to_csv(data):
 
     # Remove duplicates based on 'symbol' and 'date'
     df_combined = pd.concat([df_existing, df_new])
-    df_combined.replace("-", np.nan, inplace=True)
-    df_combined.fillna(method='ffill', inplace=True)
+    df_combined.replace("-", 0, inplace=True)
+    #df_combined.fillna(method='ffill', inplace=True)
 
     df_combined = df_combined.drop_duplicates(subset=["date"], keep="last")
 
