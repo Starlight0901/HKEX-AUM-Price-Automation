43e8bb6af71471135d0c6b90c73791271fb14718
Shamini Fernando <126609804+Starlight0901@users.noreply.github.com>
2025-02-20 16:31:53 +0530

    Update date_change.py

--- date_change.py
+++ date_change.py
@@ -24,7 +24,7 @@ df.columns = ["date", "volume_9008", "volume_9042", "volume_9439", "turnover_900
 df = df[["date", "volume_9008", "turnover_9008", "volume_9042", "turnover_9042", "volume_9439", "turnover_9439"]]
 
 # Save the modified CSV
-df.to_csv("formatted_file.csv", index=False)
+df.to_csv("volume_turnover_aum_data - Backfill Try 2 full doc.csv", index=False)
 
 print("CSV file has been reformatted and saved as formatted_file.csv")
 
